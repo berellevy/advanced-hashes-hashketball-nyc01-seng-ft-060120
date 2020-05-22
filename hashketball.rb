7a71f2ebdd500ddfe0164530e7184af7238a9925
Berel Levy <berellevy@gmail.com>
2020-05-22 22:48:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -196,7 +196,7 @@ def big_shoe_rebounds
       if v_player_hash[:shoe] > lagrest_dang_shoe
         largest_dang_shoe = v_player_hash[:shoe]
         rebounds << v_player_hash[:rebounds]
-        
+
       end
     end
 
